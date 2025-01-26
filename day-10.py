33280570719c9deb2a536de32ac7f04956016ccf
param-dholakia <paramdholakia3@gmail.com>
2025-01-26 09:45:33 +0530

    day-10

---
+++ day-10.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    #Function to find the sum of contiguous subarray with maximum sum.
+    def maxSubArraySum(self,arr):
+        ##Your code here
+        msf=arr[0]
+        meh=arr[0]
+        for i in range(1,len(arr)):
+            msf=max(arr[i],msf+arr[i])
+            meh=max(msf,meh)
+        return meh
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+
+        ob = Solution()
+
+        print(ob.maxSubArraySum(arr))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
